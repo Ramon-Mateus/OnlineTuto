f42ebbd059e9fe0bd8e4f658cc33dc0ec80e47ed
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-09-10 16:03:09 -0300

    Session page restrictions

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -2,6 +2,12 @@
     Inherits Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-
+        If Session("role") IsNot Nothing Then
+            If Not Session("role").ToString().Equals("STAFF") Then
+                Response.Redirect("Login.aspx")
+            End If
+        Else
+            Response.Redirect("Login.aspx")
+        End If
     End Sub
 End Class
--- EmployeeMgt.aspx.vb
+++ EmployeeMgt.aspx.vb
@@ -2,7 +2,13 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        If Session("role") IsNot Nothing Then
+            If Not Session("role").ToString().Equals("ADMIN") Then
+                Response.Redirect("Login.aspx")
+            End If
+        Else
+            Response.Redirect("Login.aspx")
+        End If
     End Sub
 
 End Class
